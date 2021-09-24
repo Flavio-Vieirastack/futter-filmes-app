bdba36d22718084e67adde3400a8d100a9b0de2e
Flavio Vieira <flavioextreme1@hotmail.com>
2021-09-24 15:30:50 -0300

    feito a trasição final do app, ele esta pronto

--- app_filmes/lib/application/ui/widgets/movies_card.dart
+++ app_filmes/lib/application/ui/widgets/movies_card.dart
@@ -4,6 +4,7 @@ import 'package:app_filmes/models/movie_model.dart';
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
 import 'package:intl/intl.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class MoviesCard extends StatelessWidget {
   final MovieModel movie;
@@ -36,8 +37,9 @@ class MoviesCard extends StatelessWidget {
                   child: ClipRRect(
                     borderRadius: BorderRadius.circular(20),
                     clipBehavior: Clip.antiAlias,
-                    child: Image.network(
-                      "https://image.tmdb.org/t/p/w200${movie.posterPath}",
+                    child: FadeInImage.memoryNetwork(
+                      placeholder: kTransparentImage,
+                      image: "https://image.tmdb.org/t/p/w200${movie.posterPath}",
                       width: 148,
                       height: 184,
                       fit: BoxFit.cover,
--- app_filmes/lib/modules/movie_detail/widgets/movie_detail_header.dart
+++ app_filmes/lib/modules/movie_detail/widgets/movie_detail_header.dart
@@ -2,6 +2,7 @@ import 'package:app_filmes/models/movie_detail_model.dart';
 import 'package:app_filmes/modules/movie_detail/movie_detail_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class MovieDetailHeader extends GetView<MovieDetailController> {
   final MovieDetailModel? movie;
@@ -20,7 +21,9 @@ class MovieDetailHeader extends GetView<MovieDetailController> {
             final image = movieData.urlImages[index];
             return Padding(
               padding: EdgeInsets.all(5),
-              child: Image.network(image),
+              child: FadeInImage.memoryNetwork(
+                placeholder: kTransparentImage,
+                image: image,),
             );
           },
         ),
